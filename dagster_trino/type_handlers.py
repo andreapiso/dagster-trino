667187499b6dcbbb3fa522518fc417d5a60531c6
Andrea Pisoni <andreapiso@gmail.com>
2023-03-11 17:06:38 +0800

    Add Parquet and Arrow inputs.

--- dagster_trino/type_handlers.py
+++ dagster_trino/type_handlers.py
@@ -1,15 +1,18 @@
 from dagster._core.storage.db_io_manager import DbTypeHandler, TableSlice
 from dagster import InputContext, MetadataValue, OutputContext, TableColumn, TableSchema
 from .io_manager import TrinoDbClient
+from .types import TableFilePaths, TrinoQuery
 
 from trino.exceptions import TrinoQueryError
 
-from typing import List
-
+import pandas as pd
+import pyarrow
+from pyarrow import parquet
+import fsspec
 
 class TrinoQueryTypeHandler(DbTypeHandler):
     def handle_output(
-        self, context: OutputContext, table_slice: TableSlice, obj: str, connection
+        self, context: OutputContext, table_slice: TableSlice, obj: TrinoQuery, connection
     ):
         """Stores the Trino Query in a Table."""
         try:
@@ -31,24 +34,24 @@ class TrinoQueryTypeHandler(DbTypeHandler):
 
     def load_input(
         self, context: InputContext, table_slice: TableSlice, connection
-    ) -> str:
+    ) -> TrinoQuery:
         """Loads the input as a Trino Query"""
         return TrinoDbClient.get_select_statement(table_slice)
     
     @property
     def supported_types(self):
-        return [str]
+        return [TrinoQuery]
     
-class GCSTrinoTypeHandler(DbTypeHandler):
+class FilePathTypeHandler(DbTypeHandler):
     def handle_output(
-        self, context: OutputContext, table_slice: TableSlice, obj: str, connection
+        self, context: OutputContext, table_slice: TableSlice, obj: TableFilePaths, connection
     ):
         """Loads content of files saved at the given location into a Trino managed table."""
         pass
 
     def load_input(
         self, context: InputContext, table_slice: TableSlice, connection
-    ) -> List[str]:
+    ) -> TableFilePaths:
         """Loads the paths of object storage files populating the table"""
         #TODO: work on partitioning
         query = f'''
@@ -59,9 +62,28 @@ class GCSTrinoTypeHandler(DbTypeHandler):
         except TrinoQueryError as e:
             # TODO add messaging around this functionality is supported only with the Hive connector
             raise e
-        l:List[str] = [filepath for pathlist in res for filepath in pathlist]
-        return l
+        return [filepath for pathlist in res for filepath in pathlist]
     
     @property
     def supported_types(self):
-        return [List[str], list]
+        return [TableFilePaths]
+    
+class ArrowTypeHandler(DbTypeHandler):
+    '''
+        Type Handler to load/handle pandas types by reading/writing parquet 
+        files for Trino tables backed by Parquet working against a Hive catalog.
+    '''
+    def handle_output(
+            self, context: OutputContext, table_slice: TableSlice, obj: pyarrow.Table, connection
+        ):
+        pass
+
+    def load_input(self, context: InputContext, table_slice: TableSlice, connection) -> pyarrow.Table:
+        file_paths = FilePathTypeHandler().load_input(context, table_slice, connection)
+        fs = fsspec.filesystem(protocol='gs', project='trino-catalog', token=<token>) #temporary
+        arrow_df = parquet.ParquetDataset(file_paths, filesystem=fs)
+        return arrow_df.read()
+    
+    @property
+    def supported_types(self):
+        return [pyarrow.Table]
---
+++ dagster_trino/types.py
@@ -0,0 +1,5 @@
+from typing import NewType, List
+from dagster import usable_as_dagster_type
+
+TrinoQuery = str
+TableFilePaths = List[str]
