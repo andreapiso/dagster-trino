69e52fb54f19f1d35ef59b6d5fd65bb776c26872
Andrea Pisoni <andreapiso@gmail.com>
2023-03-20 22:32:02 +0800

    Finalise docstrings.

--- dagster_trino/io_manager.py
+++ dagster_trino/io_manager.py
@@ -21,29 +21,65 @@ TRINO_DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S" #TODO: check correctness
 def build_trino_iomanager(
     type_handlers: Sequence[DbTypeHandler], default_load_type: Optional[Type] = None
 ) -> IOManagerDefinition:
-        """
-        Builds an IO manager definition that reads inputs from and writes outputs to Trino.
-        """
-        required_resource_keys = set()
-        if any(type_handler.requires_fsspec for type_handler in type_handlers):
-            required_resource_keys.add('fsspec')
+    """Builds an IO manager definition that reads inputs from and writes outputs to Trino. 
+    Args:
+        type_handlers (Sequence[DbTypeHandler]): Each handler will execute Trino Queries or translate between
+            Trino tables and an in-memory type - e.g. a Pandas DataFrame. If only
+            one DbTypeHandler is provided, it will be used as the default_load_type.
+        default_load_type (Type): When an input has no type annotation, load it as this type.
+    Returns:
+        IOManagerDefinition
+    Examples:
+        .. code-block:: python
+            from dagster_trino import build_trino_io_manager
+            from dagster_trino.type_handlers import ArrowPandasTypeHandler
+            @asset(
+                key_prefix=["my_schema"]  # will be used as the schema in Trino
+            )
+            def my_table() -> pd.DataFrame:  # the name of the asset will be the table name
+                ...
+            trino_io_manager = build_trino_io_manager([ArrowPandasTypeHandler()])
+            @repository
+            def my_repo():
+                return with_resources(
+                    [my_table],
+                    {"io_manager": trino_io_manager.configured({...})}
+                )
+    If you do not provide a schema in your configuration, Dagster will determine a schema based on the assets and ops using
+    the IO Manager. For assets, the schema will be determined from the asset key. For ops, the schema can be
+    specified by including a "schema" entry in output metadata. If none of these is provided, the schema will
+    default to "public".
+    .. code-block:: python
+        @op(
+            out={"my_table": Out(metadata={"schema": "my_schema"})}
+        )
+        def make_my_table() -> pd.DataFrame:
+            ...
+    """
+    required_resource_keys = set()
+    if any(type_handler.requires_fsspec for type_handler in type_handlers):
+        required_resource_keys.add('fsspec')
 
-        @io_manager(
-            config_schema=define_trino_config(),
-            required_resource_keys=required_resource_keys
-        )
-        def trino_io_manager(init_context):
-            return DbIOManager(
-                type_handlers=type_handlers,
-                db_client=TrinoDbClient(),
-                io_manager_name="TrinoIoManager",
-                database=init_context.resource_config["catalog"],
-                schema=init_context.resource_config.get("schema"),
-                default_load_type=default_load_type,
-        )
-        return trino_io_manager
+    @io_manager(
+        config_schema=define_trino_config(),
+        required_resource_keys=required_resource_keys
+    )
+    def trino_io_manager(init_context):
+        return DbIOManager(
+            type_handlers=type_handlers,
+            db_client=TrinoDbClient(),
+            io_manager_name="TrinoIoManager",
+            database=init_context.resource_config["catalog"],
+            schema=init_context.resource_config.get("schema"),
+            default_load_type=default_load_type,
+    )
+    return trino_io_manager
 
 class TrinoDbClient(DbClient):
+    """
+    Trino Client class. Should not be used directly, rather through the
+    `build_trino_io_manager` function.
+    """
     @staticmethod
     @contextmanager
     def connect(context, table_slice):
--- dagster_trino/resources.py
+++ dagster_trino/resources.py
@@ -74,7 +74,7 @@ class TrinoConnection:
         Examples:
             .. code-block:: python
                 @op(required_resource_keys={"trino"})
-                def drop_database(context):
+                def drop_table(context):
                     context.resources.trino.execute_query(
                         "DROP TABLE IF EXISTS MY_TABLE"
                     )
@@ -114,6 +114,10 @@ def _create_fsspec_filesystem(config) -> fsspec.spec.AbstractFileSystem:
     return fsspec.filesystem(**fsspec_params) 
 
 class FsSpec:
+    """A Class that creates an fsspec filesystem for the desired storage protocol. 
+    In general this class should not be directly instantiated, but rather used as a 
+    resource with a type handler that requires access to the Trino underlying storage.
+    """
     def __init__(self, tmp_path, fsspec_params):
         if "protocol" not in fsspec_params:
             #default to local filesystem if none provided
@@ -124,6 +128,16 @@ class FsSpec:
         self.fs.makedirs(self.tmp_folder, exist_ok=True)
 
 def build_fsspec_resource(fsspec_params) -> ResourceDefinition:
+    """
+    Builder for fsspec filesystem with the given parameters.
+
+    Arguments:
+
+        fsspec_params(dict): Dictionary containing arguments to be 
+            passed as-is to a `fsspec_filesystem` initiation. 
+            if the `protocol` parameter is not set, it defaults to the 
+            local storage protocol `file`.
+    """
     @resource(config_schema={
         'tmp_path': Field(StringSource, is_required=True,
                         description="Path where to stage temporary files. \
